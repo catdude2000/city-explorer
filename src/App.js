747df06f556b24fe32f1a6da83edad721075fe90
Michael Pace <michael.g.pace@gmail.com>
2022-05-18 13:11:11 -0500

    app file filled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @file App.js
+ * @author Mike Pace
+ * @description 
+ */
 
-function App() {
+import React from 'react';
+import { render } from '@testing-library/react';
+import axios from 'axios';
+
+
+class App extends React.Component {
+
+/**
+ * sets the search query and location state attributes
+ * @param {object} props - the properties object
+ */
+  consturctor(props) {
+    super(props);
+    this.state= {
+      searchQuery: '',
+      location: { place_id: 'unknown', display_name: 'None' },
+    };
+  }
+
+ /**
+   * This takes no arguments, but gets a location and makes an api call.
+   * Requires npm install axios
+   * get the access key from your LocationIQ account
+   * environment variable name must begin with REACT_APP_
+   * Store the access token in .env
+   */
+  getLocation = async () => {
+    const API =`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_TOKEN}&q=${this.state.searchQuery}&format=json`;
+    const res = await axios.get(API);
+    this.setState({ location: res.data[[0] }); // 
+  };
+
+render() {
+  console.log(`Location: ${JSON.stringify(this.state.location)}`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input
+    onChange={(e) => this.setState({ searchQuery: e.target.value})}
+    placeholder='search for a city'
+    />
+    <button onClick={this.getlocation}>Explore!</button>
+    {this.state.location.place_id && (
+      <h2>The city is: {this.state.location.display_name}</h2>
+    )}
+    </>
   );
+}
 }
 
 export default App;
