8f1578a4f22261876706ecad4c3458e650672d16
Michael Pace <michael.g.pace@gmail.com>
2023-01-25 02:19:39 +0000

    form for input button added, renders on page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import './App.css';
 import React from 'react';
-// import axios from 'axios';
-// let API_KEY =
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { Form } from 'react-bootstrap';
+let API_KEY = process.env.REACT_APP_LOC_APIKEY;
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-
-    }
+      city: "",
+      cityData: {},
+      error: false,
+      errorMessage: ""
+    };
   }
+
+  submitCityHandler = async (event) => {
+    event.preventDefault();
+    try{
+      let url= `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.city}&format=json`;
+
+      let cityInfo = await axios.get(url);
+
+      this.setState({
+        cityData: cityInfo.data[0],
+        error: false
+      });
+
+    } catch (error) {
+      this.setState({
+
+        error: true,
+        errorMessage: `An error ocurred: ${error.response.status}`
+      });
+    }
+  };
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+  };
+
   render() {
-    return 
+
+    console.log("city", this.state.cityData);
+    return (
       <>
-      
+      <form id="form" onSubmit={this.submitCityHandler}>
+        <label>
+          {" "}
+          Pick a City:
+          <input type="text" onInput={this.handleCityInput} />
+        </label>
+        <button type="submit">Get City Data</button>
+      </form>
+
       </>
+      );
   }
 }
 export default App;
